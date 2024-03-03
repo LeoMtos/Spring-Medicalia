f16e9bbf7caaddc03e286015d47d67f455a2440e
LeoMtos <leomtos@outlook.com>
2024-03-03 17:15:03 -0600

    cambios

--- src/main/java/com/medicalia/spring/medicalia/security/filters/JwtAuthorizationFilter.java
+++ src/main/java/com/medicalia/spring/medicalia/security/filters/JwtAuthorizationFilter.java
@@ -19,7 +19,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
-
 @RequiredArgsConstructor
 @Component
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
